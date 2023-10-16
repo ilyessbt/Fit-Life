e112dc3025bdb7f730c0a426225e834cc01c8bce
Ilyess BEN TEKAYA <ilyessbtekaya04@gmail.com>
2023-10-16 10:19:51 +0100

    cross origin

--- src/main/java/com/example/fitlife/controller/CourseController.java
+++ src/main/java/com/example/fitlife/controller/CourseController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.*;
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/courses")
 public class CourseController {
